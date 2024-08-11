f6ff1694cdd57d8a5198c0b9e61d6985c3c06662
Ubuntu <ubuntu@ip-172-31-14-29.ap-south-1.compute.internal>
2024-08-11 09:54:19 +0000

    Remote Backend

---
+++ remote/variables.tf
@@ -0,0 +1,12 @@
+# Backend Variables
+variable "state_bucket_name" {
+    default = "terraweek-demo-state-bucket11"
+}
+
+variable "state_table_name" {
+    default = "terraweek-demo-state-table11"
+}
+
+variable "aws_region" {
+    default = "us-east-1"
+}
